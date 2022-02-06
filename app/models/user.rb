47e2b24b3f0fe6d495909feecc34f2032f6f9205
hachiyomo <yomohachi09@gmail.com>
2022-02-06 04:05:00 +0000

    likes1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,20 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-
+  
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+ 
+  
 def follow(other_user)
-  unless self == other_user
+    unless self == other_user # 自分自身でないか
     self.relationships.find_or_create_by(follow_id: other_user.id)
-  end
+    end
 end
 
 def unfollow(other_user)
@@ -31,4 +36,21 @@ def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
 end
   
+# 追加  
+def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+end
+
+def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+end
+
+def favo?(micropost)
+   self.likes.include?(micropost)
+end
+
+# def feed_microposts
+#     Micropost.where(user_id: self.favopost_ids + [self.id])
+# end
 end
